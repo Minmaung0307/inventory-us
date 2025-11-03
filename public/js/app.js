8229b55fe9a80e3abf616aa9b6bf312385c8ad8b
Min Maung <minmaung0307@gmail.com>
2025-11-03 06:13:58 -0500

    added userGuide page

--- public/js/app.js
+++ public/js/app.js
@@ -238,6 +238,7 @@
 
           <div class="footer" style="flex-direction:column;gap:8px;padding-bottom:16px">
             <div style="display:flex;gap:10px">
+              <a href="/userGuide.html" target="_blank">User Guide</a>
               <a href="https://youtube.com"  target="_blank" rel="noopener" title="YouTube"><i class="ri-youtube-fill"></i></a>
               <a href="https://facebook.com" target="_blank" rel="noopener" title="Facebook"><i class="ri-facebook-fill"></i></a>
               <a href="https://instagram.com" target="_blank" rel="noopener" title="Instagram"><i class="ri-instagram-line"></i></a>
